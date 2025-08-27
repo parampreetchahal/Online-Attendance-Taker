c0b4a69ce19ca9a4f20f766762d984b40a577dc3
Parampreet Singh Chahal <164636326+parampreetchahal@users.noreply.github.com>
2025-08-27 23:31:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def process_csv(file, valid_minutes, format):
         section_roll_df = pd.read_sql(query, engine)
         return section_roll_df
 
-    db_connection_string = 'mysql+pymysql://root:@127.0.0.1:3306/collegeid'
+    db_connection_string = 'mysql+pymysql://root:@127.0.0.1:3306/YOUR_DB_NAME'
     full_names = df_filtered['Full Name'].tolist()
     section_roll_df = fetch_section_and_roll(full_names, db_connection_string)
     df_final = pd.merge(df_filtered, section_roll_df, left_on='Full Name', right_on='name', how='left')
@@ -132,3 +132,4 @@ def upload_file():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
